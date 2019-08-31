61d151e41cb4686a1d5e55a3314b33df45b27550
Alex Puzyk <APuzyk@gmail.com>
2019-08-31 13:14:47 -0700

    Updating sample.py with more info

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup
 
 setup(
     name='music_review_modeling',
+    version='0.0.1',
+    url='https://github.com/APuzyk/music_review_modelling',
+    python_requires='>=3.5',
+    install_requires=['keras'],
+
     entry_points={
         'console_scripts': [
             'music_review_modeling=command_line:main',
