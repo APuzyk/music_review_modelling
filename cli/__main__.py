51f8b055ae09fda1ceb13a85ae4761b6738e8695
Alex Puzyk <APuzyk@gmail.com>
2019-09-21 10:10:22 -0700

    adding betters args

--- cli/__main__.py
+++ cli/__main__.py
@@ -4,12 +4,13 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser("A module for creating text neural networks for music reviews")
-    parser.add_argument("config", help="The config yaml to train the model", type=str)
-    parser.add_argument("training", help="Are we training?  If not we'll be predicting", type=bool, default=True)
-    parser.add_argument("test", help="Is this being run for testing", type=bool, default=False)
+    parser.add_argument("-c", "--config", help="The config yaml to train the model", type=str, dest="config")
+    parser.add_argument("-t", "--train", help="Are we training?  If not we'll be predicting", type=bool, default=True,
+                        dest="train")
+    parser.add_argument("--test", help="Is this being run for testing", type=bool, default=False, dest="test")
     args = parser.parse_args()
     if args.training:
-        t = Trainer(args.config, args.is_test)
+        t = Trainer(args.config, args.test)
         t.train_model()
 
 
