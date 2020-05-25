94d9ebb66aa1f032811f1dcd66af6d56a87280eb
Alex Puzyk <APuzyk@gmail.com>
2020-05-24 20:38:15 -0700

    Update architecture for CNN to pytorch

--- models/text_cnn.py
+++ models/text_cnn.py
@@ -1,5 +1,6 @@
-from keras.layers import Input, Embedding, Conv2D, MaxPooling2D, Reshape, Flatten, Dense, Concatenate, Dropout
-from keras import Model
+import torch.nn as nn
+from torch import from_numpy, flatten, cat
+import torch.nn.functional as F
 from models.text_nn import TextNN
 
 
@@ -9,39 +10,52 @@ class TextCNN(TextNN):
         super(TextCNN, self).__init__()
 
         self.text_input_size = text_input_size
+
+        # create layers
+        self.embedding = nn.Embedding(embedding_mat.shape[0], embedding_mat.shape[1])
+        self.create_embedding_layer(from_numpy(embedding_mat))
+
         self.ngram_filters = ngram_filters
-        self.embedding_mat = embedding_mat
+        c2d_out_dim = self.create_conv_layers(ngram_filters)
+
+        self.fc1 = nn.Linear(c2d_out_dim, 100)
+        self.dropout = nn.Dropout(p=0.2)
+        self.fc2 = nn.Linear(100, 2)
 
         self.model_type = 'TextCNN'
-        self.build_model()
 
-    def build_model(self):
-        inputs = Input(shape=(self.text_input_size,))
+    def create_embedding_layer(self, embedding_mat):
+        self.embedding = nn.Embedding(embedding_mat.shape[0], embedding_mat.shape[1])
+        self.embedding.weight.data.copy_(embedding_mat)
+        self.embedding.requires_grad_(requires_grad=False)
 
-        word_mat = Embedding(self.embedding_mat.shape[0],
-                             self.embedding_mat.shape[1],
-                             weights=[self.embedding_mat],
-                             input_length=(self.text_input_size,), trainable=False)(inputs)
+    def create_conv_layers(self, ngram_filters):
+        out_dim = 0
+        for i in ngram_filters:
+            self.__setattr__(f"c2d_ngram_{i}",
+                             nn.Conv2d(1, 300,
+                                       kernel_size=(i, int(self.embedding.embedding_dim)),
+                                       stride=(1, int(self.embedding.embedding_dim))))
+            self.__setattr__(f"pool_ngram_{i}",
+                             nn.MaxPool2d(stride=(300, 1), kernel_size=(300, 1)))
+            out_dim += self.text_input_size - i + 1
+        return out_dim
 
-        x_3d = Reshape(target_shape=(self.text_input_size, self.embedding_mat.shape[1], 1))(word_mat)
-        conv_layers = []
-
+    def forward(self, x):
+        batch_size = x.shape[0]
+        x = self.embedding(x)
+        x = x.view(batch_size, 1, self.text_input_size, self.embedding.embedding_dim)
+        layers = []
         for i in self.ngram_filters:
-            x = Conv2D(filters=300, kernel_size=(i, int(word_mat.shape[2])), strides=(1, int(word_mat.shape[2])))(x_3d)
-            x = Reshape((int(x.shape[1]), 300, 1))(x)
-            x = MaxPooling2D(strides=(300, 1))(x)
-            x = Flatten()(x)
-            conv_layers.append(x)
+            l = self.__getattr__(f"c2d_ngram_{i}")(x)
+            l = F.relu(l)
+            l = l.permute(0, 3, 1, 2)
+            l = self.__getattr__(f"pool_ngram_{i}")(l)
+            l = flatten(l, 1)
+            layers.append(l)
+        x = cat(layers, 1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
 
-        x = Concatenate()(conv_layers)
-
-        x = Dense(100)(x)
-
-        x = Dropout(rate=0.2)(x)
-
-        predictions = Dense(2, activation='softmax')(x)
-
-        model = Model(inputs=inputs, outputs=predictions)
-
-        model.compile(optimizer='adam', loss='categorical_crossentropy')
-        self.model = model
+        return x
