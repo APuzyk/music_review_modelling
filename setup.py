d91fb50594bcccc9eea221fff373c92aae017813
Alex Puzyk <APuzyk@gmail.com>
2020-05-25 19:54:06 -0700

    updating install requirements for torch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     url='https://github.com/APuzyk/music_review_modelling',
     python_requires='>=3.5',
-    install_requires=['keras', 'PyYAML', 'gensim', 'numpy', 'scikit-learn'],
+    install_requires=['torch', 'gensim', 'numpy', 'scikit-learn'],
     packages=find_packages(),
     entry_points={
         'console_scripts': [
