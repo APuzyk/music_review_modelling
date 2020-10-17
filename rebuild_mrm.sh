99aefe1cd26fa3b87923f78eb71cd1adac9c5a47
Alex Puzyk <APuzyk@gmail.com>
2020-10-17 10:44:07 -0700

    Removing personal bash script

--- rebuild_mrm.sh
+++
@@ -1,4 +0,0 @@
-pip uninstall mr_modeling
-rm -r ~/Projects/music_reviews/music_review_modeling/dist
-python3 ~/Projects/music_reviews/music_review_modeling/setup.py sdist bdist_wheel
-pip install ~/Projects/music_reviews/music_review_modeling/dist/mr_modeling-0.0.1-py3-none-any.whl
