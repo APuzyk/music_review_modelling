f080bbfa1138b22ff6cb17d88694ba75878b3dc9
Alex Puzyk <APuzyk@gmail.com>
2020-10-28 19:14:17 -0700

    Bug fixes for prediction

--- mr_modeling/predictor/predictor.py
+++ mr_modeling/predictor/predictor.py
@@ -10,14 +10,15 @@ class Predictor:
     
     def get_prediction(self, text: str):
         text_vec = self.get_text_vector(text)
-        return self.model(from_numpy(text_vec))
+        preds = self.model(from_numpy(text_vec))
+        return [float(p) for p in preds[0]]
     
     def get_text_vector(self, text):
         text = self.clean_text(text)
         #TODO: how do we handle unknown values
         indexes = [self.word_dict.get(word, 0) for word in text]
         indexes = self.vectorize_text(indexes)
-        indexes = [indexes] # add dim for batch
+        indexes = np.array([indexes]) # add dim for batch
 
         return indexes
         
@@ -36,4 +37,4 @@ class Predictor:
         else:
             [word_indexes.append(0) for _ in range(self.model.text_input_size - len(word_indexes))]
         
-        return np.array(word_indexes)
+        return word_indexes
